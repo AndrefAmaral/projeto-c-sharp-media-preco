1bff7ab9511b9b60d169d38613b79050d6938d3c
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-03-13 17:19:09 -0300

    Exercicio de vetores

---
+++ Product.cs
@@ -0,0 +1,8 @@
+namespace Media_Preco 
+{
+    class Product
+    {
+        public string Name {get; set;}
+        public double Price {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media_Preco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int n = int.Parse (Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+            
+            double avg = sum / n;
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
